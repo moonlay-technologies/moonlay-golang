caed3e461eca14a19957b0557453d303a1c8774d
Putra Fajar <putrautalls@gmail.com>
2023-02-15 14:48:00 +0700

    fix typo in bson delivery order log model

--- app/models/delivery_order_log_model.go
+++ app/models/delivery_order_log_model.go
@@ -1,15 +1,16 @@
 package models
 
 import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"order-service/global/utils/model"
 	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type DeliveryOrderLog struct {
 	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	RequestID string             `json:"request_id,omitempty" bson:"request_id,omitempty"`
-	DoCode    string             `json:"do_code,omitempty" bson:"so_code,omitempty"`
+	DoCode    string             `json:"do_code,omitempty" bson:"do_code,omitempty"`
 	Data      interface{}        `json:"data,omitempty" bson:"data,omitempty"`
 	Status    string             `json:"status,omitempty" bson:"status,omitempty"`
 	CreatedAt *time.Time         `json:"created_at,omitempty" bson:"created_at,omitempty"`
