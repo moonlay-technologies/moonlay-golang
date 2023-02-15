e1979fe1566500d842cdf210fb4b63b8d6787982
Falih Nurhadi - PARTNER CST <cst_dev110@unitedtractors.com>
2023-02-15 17:55:35 +0700

    Fix: Update Start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 #bin/bash
 aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 650142038379.dkr.ecr.ap-southeast-1.amazonaws.com
-docker pull 650142038379.dkr.ecr.ap-southeast-1.amazonaws.com/order-service:"$DEPLOYMENT_GROUP_NAME"-latest
-docker run --restart always --log-opt max-size=10m --log-opt max-file=5 -d -p 8000:8000 --name order-service 650142038379.dkr.ecr.ap-southeast-1.amazonaws.com/order-service:"$DEPLOYMENT_GROUP_NAME"-latest
+docker pull 650142038379.dkr.ecr.ap-southeast-1.amazonaws.com/poc-order-service:"$DEPLOYMENT_GROUP_NAME"-latest
+docker run --restart always --log-opt awslogs-stream=order-service-api -d -p 8000:8000 --name order-service 650142038379.dkr.ecr.ap-southeast-1.amazonaws.com/poc-order-service:"$DEPLOYMENT_GROUP_NAME"-latest
