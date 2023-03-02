893a05308c5322a9c612616e44f61229240e4d5e
Falih Nurhadi - PARTNER CST <cst_dev110@unitedtractors.com>
2023-03-02 11:16:40 +0700

    Fix: Validate SO ID

--- app/controllers/delivery_order_controller.go
+++ app/controllers/delivery_order_controller.go
@@ -82,6 +82,11 @@ func (c *deliveryOrderController) Create(ctx *gin.Context) {
 
 	mustActiveField := []*models.MustActiveRequest{
 		{
+			Table:    "sales_orders",
+			ReqField: "sales_order_id",
+			Clause:   fmt.Sprintf("id = %d AND deleted_at IS NULL", insertRequest.SalesOrderID),
+		},
+		{
 			Table:    "warehouses a JOIN agents b JOIN sales_orders c ON a.owner_id = b.id AND c.agent_id = b.id",
 			ReqField: "warehouse_owner_id",
 			Clause:   fmt.Sprintf("c.id = %d AND b.deleted_at IS NULL AND a.`status` = 1", insertRequest.SalesOrderID),
