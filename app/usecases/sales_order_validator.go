2d4819090c2a11f8a95b70ab804aab7586bfce4e
Putra Fajar <putrautalls@gmail.com>
2023-03-19 16:51:21 +0700

    refactor code getSODetailAll, getSODetailBySoID, GetSOSyncToKafkaHistories, GetSoJourneyEventLog

--- app/usecases/sales_order_validator.go
+++ app/usecases/sales_order_validator.go
@@ -10,6 +10,7 @@ import (
 	"order-service/global/utils/helper"
 	baseModel "order-service/global/utils/model"
 	"strconv"
+	"strings"
 
 	"github.com/bxcodec/dbresolver"
 	"github.com/gin-gonic/gin"
@@ -17,6 +18,8 @@ import (
 
 type SalesOrderValidatorInterface interface {
 	GetSalesOrderValidator(*gin.Context) (*models.SalesOrderRequest, error)
+	GetSalesOrderDetailValidator(*gin.Context) (*models.GetSalesOrderDetailRequest, error)
+	GetSalesOrderSyncToKafkaHistoriesValidator(*gin.Context) (*models.SalesOrderEventLogRequest, error)
 }
 
 type SalesOrderValidator struct {
@@ -159,6 +162,170 @@ func (c *SalesOrderValidator) GetSalesOrderValidator(ctx *gin.Context) (*models.
 		VillageID:         intVillageID,
 		StartCreatedAt:    startCreatedAt,
 		EndCreatedAt:      endCreatedAt,
+	}
+	return salesOrderRequest, nil
+}
+
+func (c *SalesOrderValidator) GetSalesOrderDetailValidator(ctx *gin.Context) (*models.GetSalesOrderDetailRequest, error) {
+	var result baseModel.Response
+
+	pageInt, err := c.getIntQueryWithDefault("page", "1", true, ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	perPageInt, err := c.getIntQueryWithDefault("per_page", "10", true, ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	sortField := c.getQueryWithDefault("sort_field", "created_at", ctx)
+
+	if sortField != "order_status_id" && sortField != "so_date" && sortField != "so_ref_code" && sortField != "so_code" && sortField != "store_code" && sortField != "store_name" && sortField != "created_at" && sortField != "updated_at" {
+		err = helper.NewError("Parameter 'sort_field' harus bernilai 'order_status_id' or 'so_date' or 'so_ref_code' or 'so_code' or 'store_code' or 'store_name' or 'created_at' or 'updated_at' ")
+		result.StatusCode = http.StatusBadRequest
+		result.Error = helper.WriteLog(err, http.StatusBadRequest, err.Error())
+		ctx.JSON(result.StatusCode, result)
+		return nil, err
+	}
+
+	intAgentID, err := c.getIntQueryWithDefault("agent_id", "0", false, ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	intStoreID, err := c.getIntQueryWithDefault("store_id", "0", false, ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	intBrandID, err := c.getIntQueryWithDefault("brand_id", "0", false, ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	intOrderSourceID, err := c.getIntQueryWithDefault("order_source_id", "0", false, ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	intOrderStatusID, err := c.getIntQueryWithDefault("order_status_id", "0", false, ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	intID, err := c.getIntQueryWithDefault("id", "0", false, ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	intProductID, err := c.getIntQueryWithDefault("product_id", "0", false, ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	intCategoryID, err := c.getIntQueryWithDefault("category_id", "0", false, ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	intSalesmanID, err := c.getIntQueryWithDefault("salesman_id", "0", false, ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	intProvinceID, err := c.getIntQueryWithDefault("province_id", "0", false, ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	intCityID, err := c.getIntQueryWithDefault("city_id", "0", false, ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	intDistrictID, err := c.getIntQueryWithDefault("district_id", "0", false, ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	intVillageID, err := c.getIntQueryWithDefault("village_id", "0", false, ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	dateFields := []*models.DateInputRequest{}
+
+	startSoDate, dateFields := c.getQueryWithDateValidation("start_so_date", "", dateFields, ctx)
+
+	endSoDate, dateFields := c.getQueryWithDateValidation("end_so_date", "", dateFields, ctx)
+
+	startCreatedAt, dateFields := c.getQueryWithDateValidation("start_created_at", "", dateFields, ctx)
+
+	endCreatedAt, dateFields := c.getQueryWithDateValidation("end_created_at", "", dateFields, ctx)
+
+	err = c.requestValidationMiddleware.DateInputValidation(ctx, dateFields, constants.ERROR_ACTION_NAME_GET)
+	if err != nil {
+		return nil, err
+	}
+
+	salesOrderRequest := &models.GetSalesOrderDetailRequest{
+		Page:              pageInt,
+		PerPage:           perPageInt,
+		SortField:         sortField,
+		SortValue:         c.getQueryWithDefault("sort_value", "desc", ctx),
+		GlobalSearchValue: c.getQueryWithDefault("global_search_value", "", ctx),
+		AgentID:           intAgentID,
+		StoreID:           intStoreID,
+		BrandID:           intBrandID,
+		OrderSourceID:     intOrderSourceID,
+		OrderStatusID:     intOrderStatusID,
+		StartSoDate:       startSoDate,
+		EndSoDate:         endSoDate,
+		ID:                intID,
+		ProductID:         intProductID,
+		CategoryID:        intCategoryID,
+		SalesmanID:        intSalesmanID,
+		ProvinceID:        intProvinceID,
+		CityID:            intCityID,
+		DistrictID:        intDistrictID,
+		VillageID:         intVillageID,
+		StartCreatedAt:    startCreatedAt,
+		EndCreatedAt:      endCreatedAt,
+	}
+	return salesOrderRequest, nil
+}
+
+func (c *SalesOrderValidator) GetSalesOrderSyncToKafkaHistoriesValidator(ctx *gin.Context) (*models.SalesOrderEventLogRequest, error) {
+
+	pageInt, err := c.getIntQueryWithDefault("page", "1", true, ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	perPageInt, err := c.getIntQueryWithDefault("per_page", "10", true, ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	sortField := c.getQueryWithDefault("sort_field", "created_at", ctx)
+
+	intAgentID, err := c.getIntQueryWithDefault("agent_id", "0", false, ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	salesOrderRequest := &models.SalesOrderEventLogRequest{
+		Page:              pageInt,
+		PerPage:           perPageInt,
+		SortField:         sortField,
+		SortValue:         strings.ToLower(c.getQueryWithDefault("sort_value", "desc", ctx)),
+		GlobalSearchValue: c.getQueryWithDefault("global_search_value", "", ctx),
+		RequestID:         c.getQueryWithDefault("request_id", "", ctx),
+		SoCode:            c.getQueryWithDefault("so_code", "", ctx),
+		Status:            c.getQueryWithDefault("status", "", ctx),
+		Action:            c.getQueryWithDefault("action", "", ctx),
+		AgentID:           intAgentID,
 	}
 	return salesOrderRequest, nil
 }
