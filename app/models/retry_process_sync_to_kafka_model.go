5b9baac9154c00eed4e08a57cd1c462c273a6341
Adia Mahardika <adia.mahardika@moonlay.com>
2023-04-03 16:54:20 +0700

    Edit response

--- app/models/retry_process_sync_to_kafka_model.go
+++ app/models/retry_process_sync_to_kafka_model.go
@@ -8,6 +8,5 @@ type SORetryProcessSyncToKafkaResponse struct {
 
 type DORetryProcessSyncToKafkaResponse struct {
 	DeliveryOrderLogEventId string `json:"delivery_order_event_logs_id,omitempty" bson:"delivery_order_event_logs_id,omitempty"`
-	Status                  string `json:"status,omitempty" bson:"status,omitempty"`
 	Message                 string `json:"message,omitempty" bson:"message,omitempty"`
 }
